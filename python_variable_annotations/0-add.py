896807453a224ae4a1f9f7aa52086bf208424c23
MateoOlv <6304@holbertonstudents.com>
2023-08-29 08:12:25 -0700

    pycode style

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,8 +4,8 @@ Add function return sum of a + b
 """
 
 
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """
     get two values and sum
     """
-    return(a+b)
+    return (a+b)
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,8 +4,8 @@ Add function return two string conc
 """
 
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     get two strings
     """
-    return(str1 + str2)
+    return (str1 + str2)
