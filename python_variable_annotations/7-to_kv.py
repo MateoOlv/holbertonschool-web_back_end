705805915aaf178ba58abaa6ac854cb22d3bea7e
MateoOlv <6304@holbertonstudents.com>
2023-08-29 14:09:10 -0700

    : extra

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,8 +6,6 @@ takes list and return sum
 """
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]::
-    """
-    sum list
-    """
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """sum list"""
     return (k, float(v * v))
