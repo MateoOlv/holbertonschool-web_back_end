e397368d992e0be44daf70b7a5632e69e9b874e7
MateoOlv <6304@holbertonstudents.com>
2023-08-29 08:10:10 -0700

    comm

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
-"""Add function return sum of a + b"""
+"""
+Add function return sum of a + b
+"""
+
+
 def add(a:float, b:float) -> float:
+    """
+    get two values and sum
+    """
     return(a+b)
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+Add function return two string conc
+"""
+
+
 def concat(str1:str, str2:str) -> str:
+    """
+    get two strings
+    """
     return(str1 + str2)
