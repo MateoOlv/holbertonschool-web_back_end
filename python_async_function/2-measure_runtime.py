3745ad8ff94ba28848aab2fed3ff0f428a9bdf87
MateoOlv <6304@holbertonstudents.com>
2023-09-03 11:53:54 -0700

    2 task

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,8 +8,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """function"""
-    start = time()
+    start = stime()
     run(wait_n(n, max_delay))
-    end = time()
-    time = end - start
-    return time / n
+    end = stime()
+    stime = end - start
+    return stime / n
