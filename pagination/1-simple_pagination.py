4e4e3d998cdb6efba8e1041f730aed960224fd64
MateoOlv <6304@holbertonstudents.com>
2023-09-06 07:09:02 -0700

    1-simple_pagination.py

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        range: Tuple = index_range(page, page_size)
+        pagination: List = self.dataset()
+
+        return (pagination[range[0]:range[1]])
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """get first and last page"""
+    last_page = page * page_size
+    first_page = last_page - page_size
+    return (first_page, last_page)
