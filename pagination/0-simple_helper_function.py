a033bf770c70933c599df97c801764677116da69
MateoOlv <6304@holbertonstudents.com>
2023-09-06 05:30:10 -0700

    doc 0 task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Imports"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """get first and last page"""
     last_page = page * page_size
     first_page = last_page - page_size
     return (first_page, last_page)
