fe153ada63b1d4b30ac5b9af1925c32998b73be8
MateoOlv <6304@holbertonstudents.com>
2023-09-17 00:30:10 -0700

    es6 BASIC TASK 1try

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+export default function createEmployeesObject(departmentName, employees) {
+	const depEmployees = {
+	  [`${departmentName}`]: [...employees],
+	};
+	return depEmployees;
+}
---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+	const ReportObject = {
+	  allEmployees: employeesList,
+	  getNumberOfDepartments(employeesList) {
+		return Object.keys(employeesList).length; },
+	};
+	return ReportObject;
+}
